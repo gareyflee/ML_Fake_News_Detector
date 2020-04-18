d948391884daf26e2eb6cc38b022f8d909b06c6c
Garey Fleeman <gareyflee@gmail.com>
2020-04-17 17:44:12 -0700

    This is the starter class for our models to keep them consistent.

---
+++ src/classifier.py
@@ -0,0 +1,20 @@
+class Classifier():
+    def __init__(self, params: dictionary of hyperparameters):
+        # Params should be a dictionary of hyperparameters
+        self.params = params
+    def __str__(self):
+        # Use this for custom printing by just saying print(Class_Obj)
+        pass
+
+    def train(self, x_train: design matrix, y_train):
+        # Use this to train the model
+        pass
+
+    def cros_validation(self, k, param: string, x, y):
+        # This function should implement k-fold cross validation on the specified param
+        pass
+
+    def predict(self, x):
+        # This function should accept either a single test point or design matrix
+        # (Make sure it works for both)
+        pass

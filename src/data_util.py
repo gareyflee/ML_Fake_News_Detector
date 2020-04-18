af63577a4347ed83a08a43d5548ae86c4727fb90
Garey Fleeman <gareyflee@gmail.com>
2020-04-17 18:33:47 -0700

    Added more functionality including: train_test_split, k_fold_split, shuffle, and normalize.

--- src/data_util.py
+++ src/data_util.py
@@ -3,20 +3,30 @@ import matplotlib.pyplot as plt
 # This file should contain all data processing functions
 
 class Data:
-    def __init__(self, X, y, train_len):
+    def __init__(self, X, y, train_len=0.75):
         assert(X.shape[0] == y.shape[0], "Error, number of labels does not match number of points.")
-        self.X_all = np.array(X)
-        self.y_all = np.array(y)
+        self.X = np.array(X)
+        self.y = np.array(y)
         if train_len <= 1.0:
             train_len = int(len(y) * train_len)
         else:
             assert(train_len < len(y), "Error, training set length must be less than total number of samples.")
+        self.train_len = int(train_len)
         self.split_train_test()
 
-    def split_train_test(self):
-        pass
+    def split_train_test(self, train_len=None):
+        # Split all data into training and testing
+        # If train len is not given, self.train_len param from constructor is used
+        if train_len is None:
+            train_len = self.train_len
+        self.shuffle()
+        self.x_train = self.X[:train_len, :]
+        self.x_val = self.X[train_len:, :]
+        self.y_train = self.y[:train_len]
+        self.y_val = self.y[train_len:]
 
     def split_k(self, k):
+        # Split data into k sets and save them as their own members of the class
         self.shuffle()
         self.x_k = np.array_split(self.X, k)
         self.y_k = np.array_split(self.y, k)
@@ -36,8 +46,15 @@ class Data:
             rand_idxs = np.random.permutation(y.shape[0])
             return X[rand_idxs], y[rand_idxs]
             
-
-
+    def normalize(self, X=None, order=2):
+        # Normalize data matrix
+        # If no argument for matrix is gen, all data in class is shuffled, 
+        # otherwise the shuffled array is returned
+        if X is None:
+            self.X = (self.X.T / np.linalg.norm(self.X, ord=order, axis=1)).T
+            return None
+        else:
+            return (X.T / np.linalg.norm(X, ord=order, axis=1)).T
 
 
 def plot_accs(x_axis, train_acc, val_acc, plt_title="", x_title="Epochs", y_title="Accuracies", x_axis_log=False, save=False):
@@ -57,11 +74,3 @@ def plot_accs(x_axis, train_acc, val_acc, plt_title="", x_title="Epochs", y_titl
     else:
         plt.show()
     plt.close()
-
-def normalize(data):
-    norm = np.linalg.norm(data, ord=2, axis=1)
-    return (data.T / norm).T
-
-
-
-def split_K(x, y):

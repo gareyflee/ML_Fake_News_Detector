1f4e62e8fffbde253c5a8e60068763fa5b8a6312
Garey Fleeman <gareyflee@gmail.com>
2020-05-01 03:29:28 -0700

    Added adaboost (some parameter print statements had to be commented out because of the ab_param dictionarys.

--- src/classifier.py
+++ src/classifier.py
@@ -6,7 +6,7 @@ from sklearn.metrics import classification_report, confusion_matrix
 import numpy as np
 import sys
 import json
-import re
+import matplotlib.pyplot as plt
 
 class Classifier():
     model_types = ["fact", "bias"]
@@ -25,24 +25,28 @@ class Classifier():
         if self.best_params is not None:
             print("Building {} models with parameters found in grid search.".format(self.name))
             params = self.best_params
+            print("Accuracies: ")
             print_dict(self.best_score)
+            self.clf = self.best_clf
         else:
             print("Building {} models with parameters given in constructor.".format(self.name))
             params = self.params
-        print_dict(params)
-        self.clf = {}
-        for type in self.model_types:
-            param = params[type]
-            print(param)
-            print(self.params)
-            self.clf[type] = self.clf_func(**param)
+      
+            self.clf = {}
+            for type in self.model_types:
+                param = params[type]
+                print(param)
+                print(self.params)
+                self.clf[type] = self.clf_func(**param)
+        # print("Parameters: ")
+        # print_dict(params)
 
     def fit(self):
         print("\n----------- Fitting Models -----------")
         print("Fitting Bias Model to ", self.data.X_train.shape[0], "test points with ", self.data.X_train.shape[1], "features.")  
         for type in self.model_types:
             self.clf[type].fit(self.data.X_train, self.data.y_train[type])
-
+    
     def predict(self):
         y_pred = {}
         for type in self.model_types:
@@ -57,29 +61,34 @@ class Classifier():
             acc[type] = metrics.accuracy_score(self.data.y_test[type], y_pred[type])
         print_dict(acc)
         return acc
+    # def plot_param_sweep(self):
+    #     assert self.best_params is not None, "Error, grid search has not been performed."
+    #     print(self.best_clf.cv_results_)
+        
 
     def sweep_params(self):
         assert self.param_search_grid is not None, "Error, no grid specified."
         print("\n----------- Performing Hyperparameter Grid Search -----------")
-        print_dict(self.param_search_grid, indent=2)
+        # print_dict(self.param_search_grid, indent=2)
         X = np.vstack([self.data.X_train, self.data.X_val])
         y = {}
         clf_search = {}
         for type in self.model_types:
-            clf_search[type] = GridSearchCV(self.clf_func(), self.param_search_grid[type], refit=True)
             y[type] = np.hstack([self.data.y_train[type], self.data.y_val[type]])
-
-        for type in self.model_types:
+            clf_search[type] = GridSearchCV(self.clf_func(), self.param_search_grid[type], \
+                refit=True, return_train_score=True, verbose=1)
             clf_search[type].fit(X, y[type])
+            
         self.best_clf  = {}
         self.best_params = {}
         self.best_score = {}
+        self.results = {}
         for type in self.model_types:
             self.best_clf[type] = clf_search[type].best_estimator_
-            self.best_params[type] = clf_search[type].best_parameters_
+            self.best_params[type] = clf_search[type].best_params_
             self.best_score[type] = clf_search[type].best_score_
-        print("Best Hyperparameters and Score:")
-        print_dict(self.best_params.update(self.best_score), indent=2)
+        # print("Best Hyperparameters and Score:")
+        # print_dict(self.best_params.update(self.best_score), indent=2)
 def print_dict(dict, indent=2):
     print(json.dumps(dict, indent=indent))
   
--- src/main.py
+++ src/main.py
@@ -23,12 +23,12 @@ def main():
     params_rf = {"fact": params_fact, "bias": params_bias}
 
     params_fact_sg = {
-        "n_estimators": [1, 2, 5, 10, 50],
-        "max_depth": [1, 2, 5, 10, 20]
+        "n_estimators": [1, 2, 5],
+        "max_depth": [1, 2, 5]
     }
     params_bias_sg = {
-        "n_estimators": [1, 2, 5, 10, 50],
-        "max_depth": [1, 2, 5, 10, 20]
+        "n_estimators": [1, 2, 5],
+        "max_depth": [1, 2, 5]
     }
     params_rf_sg = {"fact": params_fact_sg, "bias": params_bias_sg}
 
@@ -48,14 +48,49 @@ def main():
     params_fact_sg = {
         "C": [0.001, 0.1, 1.0],
         "kernel": ["linear", "poly", "rbf"], 
-        "degree": [1, 2]
+        "degree": [1]
     }
     params_bias_sg = {
         "C": [0.001, 0.1, 1.0],
         "kernel": ["linear", "poly", "rbf"], 
-        "degree": [1, 2]
+        "degree": [1]
     }
     params_svc_sg = {"fact": params_fact_sg, "bias": params_bias_sg}
+
+    # Hyperparameters for Adaboost (Decision Tree - Stump) Classifier
+    from sklearn.tree import DecisionTreeClassifier
+    from sklearn.ensemble import AdaBoostClassifier
+
+    ab_params_fact = {
+        "base_estimator": DecisionTreeClassifier(max_depth=1), 
+        "n_estimators": 10, 
+        "learning_rate": 1.
+    }
+    ab_params_bias = {
+        "base_estimator": DecisionTreeClassifier(max_depth=1), 
+        "n_estimators": 10, 
+        "learning_rate": 1.
+    }
+    params_ab = {"fact": ab_params_fact, "bias": ab_params_bias}
+
+    ab_params_fact_sg = {
+        "base_estimator": [DecisionTreeClassifier(max_depth=1)], 
+        "n_estimators": [1, 2, 5], 
+        "learning_rate": [1.]
+    }
+    ab_params_bias_sg = {
+        "base_estimator": [DecisionTreeClassifier(max_depth=1)], 
+        "n_estimators": [1, 2, 5], 
+        "learning_rate": [1.]
+    }
+    params_ab_sg = {"fact": ab_params_fact_sg, "bias": ab_params_bias_sg}
+
+    # Adaboost (Decision Tree - Stumps) Classifier
+    ab_clf = Classifier(AdaBoostClassifier, data_obj, params_ab, params_ab_sg, name="Adaboost - Decision Tree Stumps")
+    ab_clf.sweep_params()
+    ab_clf.create()
+    ab_clf.fit()
+    ab_clf.evaluate()
 
 
     rf_clf = Classifier(RandomForestClassifier, data_obj, params_rf, params_rf_sg, name="Random Forest")
@@ -66,7 +101,8 @@ def main():
     rf_clf.create()
     rf_clf.fit()
     rf_clf.evaluate()
-    # rf_clf.predict()
+
+    return
     svc = Classifier(SVC, data_obj, params_svc, params_svc_sg, name="Support Vector Classifier")
     # svc.create()
     # svc.fit()
@@ -74,7 +110,7 @@ def main():
     svc.sweep_params()
     svc.create()
     svc.fit()
-    svc.predict()
+    svc.evaluate()
     
 if __name__=="__main__":
     main()
